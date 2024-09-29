a98b3053060587289ea84d6b8007e435cad442d7
courtg123 <courtg123@gmail.com>
2024-09-29 11:48:59 -0700

    colors imported and styles added, final implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import data from './colors.json';
+
 const App = () => {
-    return <h1>Hello World</h1>;
+    return (
+        <div style={{ margin: '2em' }}>
+            {data.map(color => (
+                // main color
+                <div key={color.hex}>
+                    <p>
+                        <b>
+                            {color.name} (<span style={{ color: `#${color.hex}` }}>#{color.hex}</span>)
+                        </b>
+                    </p>
+                    <div style={{ marginBottom: '1em' }}>
+                        {color.comp.map(compColor => (
+                            <div key={compColor.hex} style={{ marginLeft: '1em' }}>
+                                <p>
+                                    &bull;{compColor.name}(<span style={{ color: `#${compColor.hex}` }}>#{compColor.hex}</span>)
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default App;
